d3cc977f49601be58a45b787605c4c3d71c2629e
Laura Fernandes <lauqfernandes@gmail.com>
2022-04-08 17:06:23 -0300

    Teste Android

--- src/screens/Aula/index.jsx
+++ src/screens/Aula/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import {React, useState} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import { styles } from './styles';
 import { Cabecalho } from '../../components/Cabecalho';
